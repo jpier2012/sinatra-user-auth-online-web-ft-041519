e6008aa0b18e1a54ec366af253ffc61538ea64c2
James Pier <jpier2012@gmail.com>
2019-05-31 19:21:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
-  create_table :users do |t|
-    t.string :name
-    t.string :email
-    t.string :password
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
   end
 end
